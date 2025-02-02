537bb7a6bbff6c7bd58b1275e8aedac2335ac633
CycloneJoker <1197795731@qq.com>
2025-02-02 10:50:29 +0800

    修改docker执行方式

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# 使用环境变量更新配置文件
-sed -i "s/username: .*/username: \"$FORUM_USERNAME\"/" ./config/config.yaml
-sed -i "s/password: .*/password: \"$FORUM_PASSWORD\"/" ./config/config.yaml
-sed -i "s/bot_token: .*/bot_token: \"$TELEGRAM_BOT_TOKEN\"/" ./config/config.yaml
-sed -i "s/channel_id: .*/channel_id: \"$TELEGRAM_CHANNEL_ID\"/" ./config/config.yaml
-sed -i "s/check_interval: .*/check_interval: $CHECK_INTERVAL/" ./config/config.yaml
-sed -i "s/level: .*/level: \"$LOG_LEVEL\"/" ./config/config.yaml
+# 替换 forum 下的用户名和密码
+sed -i "/forum:/,/base_url:/ s/username: .*/username: \"$FORUM_USERNAME\"/" ./config/config.yaml
+sed -i "/forum:/,/base_url:/ s/password: .*/password: \"$FORUM_PASSWORD\"/" ./config/config.yaml
+
+# 替换 telegram 下的 bot_token 和 channel_id
+sed -i "/telegram:/,/logging:/ s/bot_token: .*/bot_token: \"$TELEGRAM_BOT_TOKEN\"/" ./config/config.yaml
+sed -i "/telegram:/,/logging:/ s/channel_id: .*/channel_id: \"$TELEGRAM_CHANNEL_ID\"/" ./config/config.yaml
 
 # 启动应用
 python run.py
